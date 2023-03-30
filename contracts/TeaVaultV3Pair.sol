d387e46e52f21d80ae84c3dc5ddd6680db87c44f
Venson <venson@teahouse.finance>
2023-03-31 06:36:25 +0800

    get underlying assets including swap fees

--- contracts/TeaVaultV3Pair.sol
+++ contracts/TeaVaultV3Pair.sol
@@ -489,8 +489,84 @@ contract TeaVaultV3Pair is
     ) internal view returns (uint256 amount0, uint256 amount1, uint256 fee0, uint256 fee1) {
         if (_index >= positions.length) revert PositionNotExist();
 
-        // TODO: implement main logic
+        Position storage position = positions[_index];
+        bytes32 positionKey = keccak256(abi.encodePacked(address(this), position.tickLower, position.tickUpper));
+        (uint160 sqrtPriceX96, int24 tick, , , , , ) = pool.slot0();
+        uint256 feeGrowthGlobal0X128 = pool.feeGrowthGlobal0X128();
+        uint256 feeGrowthGlobal1X128 = pool.feeGrowthGlobal1X128();
+        (, , uint256 feeGrowthOutside0X128Lower, uint256 feeGrowthOutside1X128Lower, , , , ) = pool.ticks(position.tickLower);
+        (, , uint256 feeGrowthOutside0X128Upper, uint256 feeGrowthOutside1X128Upper, , , , ) = pool.ticks(position.tickUpper);
+        (
+            uint128 liquidity,
+            uint256 feeGrowthInside0Last,
+            uint256 feeGrowthInside1Last,
+            uint128 tokensOwed0,
+            uint128 tokensOwed1
+        ) = pool.positions(positionKey);
+
+        (amount0, amount1) = LiquidityAmounts.getAmountsForLiquidity(
+            sqrtPriceX96,
+            TickMath.getSqrtRatioAtTick(position.tickLower),
+            TickMath.getSqrtRatioAtTick(position.tickUpper),
+            liquidity
+        );
+        
+        fee0 = tokensOwed0 + _potisionSwapFee(
+            tick,
+            position.tickLower,
+            position.tickUpper,
+            liquidity,
+            feeGrowthGlobal0X128,
+            feeGrowthInside0Last,
+            feeGrowthOutside0X128Lower,
+            feeGrowthOutside0X128Upper
+        );
+
+        fee1 = tokensOwed1 + _potisionSwapFee(
+            tick,
+            position.tickLower,
+            position.tickUpper,
+            liquidity,
+            feeGrowthGlobal1X128,
+            feeGrowthInside1Last,
+            feeGrowthOutside1X128Lower,
+            feeGrowthOutside1X128Upper
+        );
     }
+
+    function _potisionSwapFee(
+        int24 _tick,
+        int24 _tickLower,
+        int24 _tickUpper,
+        uint128 _liquidity,
+        uint256 _feeGrowthGlobalX128,
+        uint256 _feeGrowthInsideLastX128,
+        uint256 _feeGrowthOutsideX128Lower,
+        uint256 _feeGrowthOutsideX128Upper
+    ) internal pure returns (uint256 swapFee) {
+        unchecked {
+            uint256 feeGrowthInsideX128;
+            uint256 feeGrowthBelowX128;
+            uint256 feeGrowthAboveX128;
+            uint256 fixedPointQ128 = 0x100000000000000000000000000000000;
+
+            feeGrowthBelowX128 = _tick >= _tickLower?
+                _feeGrowthOutsideX128Lower:
+                _feeGrowthGlobalX128 - _feeGrowthOutsideX128Lower;
+            
+            feeGrowthAboveX128 = _tick < _tickUpper?
+                _feeGrowthOutsideX128Upper:
+                _feeGrowthGlobalX128 - _feeGrowthOutsideX128Upper;
+
+            feeGrowthInsideX128 = _feeGrowthGlobalX128 - feeGrowthBelowX128 - feeGrowthAboveX128;
+
+            swapFee = FullMath.mulDiv(
+                feeGrowthInsideX128 - _feeGrowthInsideLastX128,
+                _liquidity,
+                fixedPointQ128
+            );
+        }
+    } 
 
     /// @inheritdoc ITeaVaultV3Pair
     function allPositionInfo() external override view returns (uint256 amount0, uint256 amount1, uint256 fee0, uint256 fee1) {
