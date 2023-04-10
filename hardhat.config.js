a6b9f2e87f3c8cd5cc1cb355cf0e69a4af9b46e3
Ping-Che Chen <pcchen@kimicat.com>
2023-04-10 17:03:26 +0800

    Add hardhat-contract-sizer to easily know contract sizes.

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,7 @@ require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
+require('hardhat-contract-sizer');
 require('@openzeppelin/hardhat-upgrades');
 
 // This is a sample Hardhat task. To learn how to create your own go to
