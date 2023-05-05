ad90aa56be35c1a7578f1fb8de8f176c2e6b17ca
Venson <venson@teahouse.finance>
2023-05-05 21:29:48 +0800

    lower runs for decresing contract size

--- hardhat.config.js
+++ hardhat.config.js
@@ -30,7 +30,7 @@ module.exports = {
             viaIR: true,
             optimizer: {
                 enabled: true,
-                runs: 200,
+                runs: 199,
             },
             // debug: {
             //     revertStrings: "strip",
