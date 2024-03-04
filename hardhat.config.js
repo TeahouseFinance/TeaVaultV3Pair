6135d532adc4bb41efcd7571148f1b0bda8ee7b5
Ping-Che Chen <pcchen@kimicat.com>
2024-03-04 12:11:27 +0800

    Add mantle network to config file.

--- hardhat.config.js
+++ hardhat.config.js
@@ -69,6 +69,10 @@ module.exports = {
             url: process.env.POLYGON_URL || "",
             accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
         },
+        mantle: {
+            url: process.env.MANTLE_URL || "",
+            accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        },
         boba: {
             url: process.env.BOBA_URL || "",
             accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
